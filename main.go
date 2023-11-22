822386a8b0eb2bc070dc05b91c16ae34a32ec89d
Brian <kingbros@msn.com>
2023-11-22 15:55:14 -0800

    quick test that i have access

--- main.go
+++ main.go
@@ -10,29 +10,29 @@ import (
 
 type Film struct {
 	Event string
-	Year int64
+	Year  int64
 }
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("hello Brian")
 
-	h1 := func (w http.ResponseWriter, r *http.Request){
+	h1 := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("./src/index.html"))
 		events := map[string][]Film{
-			"Events":{
+			"Events": {
 				{Event: "The Godfather", Year: 2000},
 				{Event: "The Godfather", Year: 1998},
 				{Event: "The Godfather", Year: 1999},
 				{Event: "The Godfather", Year: 2001},
 			},
 		}
-		sort.Slice(events["Events"], func(i,j int) bool {
+		sort.Slice(events["Events"], func(i, j int) bool {
 			return events["Events"][i].Year < events["Events"][j].Year
 		})
 
 		tmpl.Execute(w, events)
 	}
-	http.HandleFunc("/",h1)
+	http.HandleFunc("/", h1)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 
 }
